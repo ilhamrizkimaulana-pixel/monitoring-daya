700a0e4f077e2d7980b3b2840dbe3b0e445f833d
ilhamrizkimaulana-pixel <ilhamrizkimaulana@gmail.com>
2026-06-08 13:25:16 +0700

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,25 +1,24 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, db
-import json
 
 def init_firebase():
     if not firebase_admin._apps:
-        # Ambil dari secrets (format simple)
         firebase_config = {
-            "type": st.secrets["firebase_type"],
-            "project_id": st.secrets["firebase_project"],
-            "private_key": st.secrets["firebase_key"],
-            "client_email": st.secrets["firebase_email"],
-            "client_id": st.secrets["firebase_client_id"],
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token",
-            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40skripsi-151e5.iam.gserviceaccount.com"
+            "type": st.secrets["firebase"]["type"],
+            "project_id": st.secrets["firebase"]["project_id"],
+            "private_key_id": st.secrets["firebase"]["private_key_id"],
+            "private_key": st.secrets["firebase"]["private_key"],
+            "client_email": st.secrets["firebase"]["client_email"],
+            "client_id": st.secrets["firebase"]["client_id"],
+            "auth_uri": st.secrets["firebase"]["auth_uri"],
+            "token_uri": st.secrets["firebase"]["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"],
+            "universe_domain": st.secrets["firebase"]["universe_domain"]
         }
         
         database_url = st.secrets["firebase_config"]["database_url"]
-        
         cred = credentials.Certificate(firebase_config)
         firebase_admin.initialize_app(cred, {"databaseURL": database_url})
 
